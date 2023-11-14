cf235feefafb51ad9d5010aac410ae89cb3ed8ce
samsks <samuel.meireles.15s@outlook.com>
2023-11-14 17:16:32 -0300

    fix: :fire: remove path

--- src/data/api.ts
+++ src/data/api.ts
@@ -2,8 +2,9 @@ import { env } from '@/env'
 
 export function api(path: string, init?: RequestInit) {
   const baseUrl = env.NEXT_PUBLIC_API_BASE_URL
+  const apiPrefix = '/api'
 
-  const url = new URL(path, baseUrl)
+  const url = new URL(apiPrefix.concat(path), baseUrl)
 
   return fetch(url, init)
 }
