b9543dcd7b15d81652282e9d30c0cae0659a1266
samsks <samuel.meireles.15s@outlook.com>
2023-11-14 18:07:28 -0300

    feat: :sparkles: dynamic home page

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,15 +1,5 @@
 import data from './data.json'
 
-export interface iProducts {
-  id: number
-  title: string
-  slug: string
-  price: number
-  image: string
-  description: string
-  featured: boolean
-}
-
 export async function GET(): Promise<Response> {
   return Response.json(data.products)
 }
---
+++ src/data/types/products.ts
@@ -0,0 +1,9 @@
+export interface iProducts {
+  id: number
+  title: string
+  slug: string
+  price: number
+  image: string
+  description: string
+  featured: boolean
+}
